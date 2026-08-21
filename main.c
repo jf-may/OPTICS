f915c4f6baa6d0dd4bb5d00e540b941dc7c23f10
jf-may <juarezferriolmay@proton.me>
2026-08-21 05:32:12 +0000

    Fixed hardcoded values in main leading to code not working. Currently works sequentially with test data.

--- main.c
+++ main.c
@@ -1,51 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 #include "optics.h"
 #include "csv_parser.h"
-
-static void print_results(const int *ordering, const double *core,
-                          const double *reach, const int n)
-{
-    printf("%-3s %-6s %-9s %-10s\n",
-           "pos", "pt_idx", "core_dist", "reach_dist");
-    printf("--- ------ --------- ----------\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("%-3d %-6d ", i, ordering[i]);
-
-        if (isinf(core[i]))   printf("%-9s ", "INF");
-        else                  printf("%-9.4f ", core[i]);
-
-        if (isinf(reach[i]))  printf("%-10s\n", "INF");
-        else                  printf("%-10.4f\n", reach[i]);
-    }
-}
 
 int main(int argc, char **argv)
 {
     if (argc < 3)
     {
-        fprintf(stderr, "Usage ./optics {input filename} {output filename}.\n");
+        fprintf(stderr, "Usage: %s <input_csv> <output_csv> <epsilon> "
+                        "<min_pts>.\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    char *input_filename = argv[1];
-    printf("\n=== Input CSV file: %s ===\n\n", input_filename);
-
-    CsvData data = load_csv(input_filename);
-    printf("Loaded %d points of dimension %d\n\n", data.n, data.dim);
-
-    ClusterOrdering results = run_optics(data.pts, data.n, 5.0, 100);
-
+    char *input_filename  = argv[1];
     char *output_filename = argv[2];
+    double epsilon        = atof(argv[3]);
+    int min_pts           = atoi(argv[4]);
+
+    printf("\n=== Input CSV file: %s ===\n\n", input_filename);
+    CsvData data = load_csv(input_filename);
+    printf("Loaded %d points of dimension %d\n", data.n, data.dim);
+
+    ClusterOrdering results = run_optics(data.pts, data.n, epsilon, min_pts);
+
     printf("\n=== Output CSV file: %s ===\n\n", output_filename);
-
-    /* For debugging */
-    print_results(results.ordering, results.core, results.reach, data.n);
-
     save_cluster_ordering_to_csv(&results, data.n, output_filename);
 
     free_cluster_ordering(&results);
