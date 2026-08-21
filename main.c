7c99f2ee30163a6898696179a1537644cf073868
jf-may <juarezferriolmay@proton.me>
2026-08-21 05:34:59 +0000

    Fixed hardcoded values in main leading to code not working. Currently works sequentially with test data.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv)
 {
-    if (argc < 3)
+    if (argc < 5)
     {
         fprintf(stderr, "Usage: %s <input_csv> <output_csv> <epsilon> "
                         "<min_pts>.\n", argv[0]);
